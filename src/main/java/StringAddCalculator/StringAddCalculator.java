1f490f9c79fdd46f2c577dbfdd4b770d51693f10
happysubin <asb0711@gmail.com>
2022-06-29 16:42:15 +0900

    TDD 문자열 계산기 두 번째 요구사항 충족

--- src/main/java/StringAddCalculator/StringAddCalculator.java
+++ src/main/java/StringAddCalculator/StringAddCalculator.java
@@ -3,7 +3,8 @@ package StringAddCalculator;
 public class StringAddCalculator {
     public int calculate(String s) {
         if(stringIsNull(s) || s.isEmpty()) return 0;
-        return 1;
+
+        return Integer.parseInt(s);
     }
 
     private boolean stringIsNull(String s){
--- src/test/java/StringAddCalculator/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculator/StringAddCalculatorTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StringAddCalculatorTest {
 
     @Test
-    @DisplayName("empty와 null이 들어갔을 때 테스트")
+    @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다")
     void empty(){
         StringAddCalculator calculator = new StringAddCalculator();
         int num1= calculator.calculate(null);
@@ -16,6 +16,15 @@ class StringAddCalculatorTest {
         assertThat(num1).isEqualTo(0);
         assertThat(num2).isEqualTo(0);
     }
-    //빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(
+
+    //영어로 일의 자리를 units라고 한다.
+
+    @Test
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
+    void units(){
+        StringAddCalculator calculator = new StringAddCalculator();
+        int num1= calculator.calculate("9");
+        assertThat(num1).isEqualTo(9);
+    }
 
 }
