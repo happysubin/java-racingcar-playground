fa25d425c5fdbca4b75a2c95c88efe515e8adcdc
happysubin <asb0711@gmail.com>
2022-06-29 16:54:04 +0900

    TDD 문자열 계산기 네 번째 요구사항 충족

--- src/main/java/StringAddCalculator/SeparationUtils.java
+++ src/main/java/StringAddCalculator/SeparationUtils.java
@@ -5,6 +5,6 @@ import java.util.ArrayList;
 public class SeparationUtils  {
 
     public String[] separate(String s) {
-        return s.split("[,]");
+        return s.split("[,:]");
     }
 }
--- src/test/java/StringAddCalculator/StringAddCalculatorTest.java
+++ src/test/java/StringAddCalculator/StringAddCalculatorTest.java
@@ -36,10 +36,17 @@ class StringAddCalculatorTest {
     @Test
     @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.(예 : “1,2”)")
     void sumWithCommas(){
-
         int num1= calculator.calculate("9,9,9");
         assertThat(num1).isEqualTo(27);
     }
+
+    @Test
+    @DisplayName("구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.")
+    void sumWithCommasAndColons(){
+        int num1= calculator.calculate("9,9,9:9");
+        assertThat(num1).isEqualTo(36);
+    }
+
 
 
 }
