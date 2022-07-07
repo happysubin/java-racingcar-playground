24e23a58797aabf70093bef0f1d69dc07db1f10a
happysubin <asb0711@gmail.com>
2022-07-08 00:31:35 +0900

    Feat: RandomUtils 클래스 구현

---
+++ src/main/java/racingcar/RandomUtils.java
@@ -0,0 +1,11 @@
+package racingcar;
+
+import java.util.Random;
+
+public class RandomUtils {
+    public static int generateRandomNumber() {
+        Random random = new Random();
+        int result = random.nextInt(10);
+        return result;
+    }
+}
---
+++ src/test/java/racingcar/RandomUtilsTest.java
@@ -0,0 +1,14 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+
+public class RandomUtilsTest {
+
+    @Test
+    @DisplayName("랜덤 숫자 생성")
+    void randomUtilsTestV1(){
+        RandomUtils randomUtils = new RandomUtils();
+    }
+}
