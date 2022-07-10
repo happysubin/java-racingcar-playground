82d15cc4acb2ae3b209fffdc1f278ee043c04936
happysubin <asb0711@gmail.com>
2022-07-10 22:52:33 +0900

    Feat: Name 래퍼 클래스 구현

--- src/main/java/refactoring/Name.java
+++ src/main/java/refactoring/Name.java
@@ -1,2 +1,14 @@
-package refactoring;public class Name {
+package refactoring;
+
+public class Name {
+
+    private String name;
+
+    public Name(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/refactoring/NameTest.java
+++ src/test/java/refactoring/NameTest.java
@@ -1,2 +1,17 @@
-package refactoring;public class NameTest {
+package refactoring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class NameTest {
+
+    @Test
+    void create() {
+        String str = "subin";
+        Name name = new Name(str);
+
+        assertThat(name.getName()).isEqualTo("subin");
+
+    }
 }
