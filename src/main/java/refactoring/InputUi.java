3b58995ac8be62203b964772c0cad4e03ff1f8fc
happysubin <asb0711@gmail.com>
2022-07-10 22:32:14 +0900

    Feat: 분리 로직 추가

--- src/main/java/refactoring/InputUi.java
+++ src/main/java/refactoring/InputUi.java
@@ -1,6 +1,8 @@
 package refactoring;
 
 
+import java.util.Scanner;
+
 public class InputUi {
     public boolean validate(String name) {
         if(name == null || name.isEmpty()){
@@ -10,5 +12,9 @@ public class InputUi {
             throw new IllegalStateException("자동차 이름은 5자를 초과할 수 없습니다.");
         }
         return true;
+    }
+
+    public String[] split(String s) {
+        return s.split(",");
     }
 }
--- src/test/java/refactoring/InputUiTest.java
+++ src/test/java/refactoring/InputUiTest.java
@@ -1,6 +1,5 @@
 package refactoring;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -19,10 +18,20 @@ public class InputUiTest {
 
     @Test
     @DisplayName("차의 이름에 공백이 들어가면 안된다.")
-    void uiTest(){
+    void uiTestV0(){
         assertThatThrownBy(() -> {
                 inputUi.validate("");
                 inputUi.validate(null);
         }).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("차의 이름은 ,로 구분한다.")
+    void uiTestV1(){
+        String[] names = inputUi.split("1,2,3,4");
+
+        assertThat(names[0]).isEqualTo("1");
+        assertThat(names[3]).isEqualTo("4");
+
     }
 }
