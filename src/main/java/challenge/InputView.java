88a9d7b6f2c93cf457ff5a94fe15b21580aba02c
happysubin <asb0711@gmail.com>
2022-07-17 02:25:16 +0900

    Feat: Input UI 로직 추가

--- src/main/java/challenge/InputView.java
+++ src/main/java/challenge/InputView.java
@@ -14,13 +14,15 @@ public class InputView {
     private static final String NUMBER_VALIDATION_FAIL_MESSAGE = "자동차 이름은 5글자 미만입니다.";
     private static final String REGEX_VALIDATION = "[0-9]";
     private static final String SPLIT_STANDARD = ",";
+    private static final String INPUT_NAME_GUIDE_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    private static final String INPUT_NUMBER_GUIDE_MESSAGE = "시도할 회수는 몇회인가요?";
 
     public  List<String> getNames() {
+        System.out.println(INPUT_NAME_GUIDE_MESSAGE);
         String nameString = getString();
         List<String> names = split(nameString);
         validate(names);
         return names;
-
     }
 
     public  String getString() {
@@ -45,6 +47,7 @@ public class InputView {
     }
 
     public int getNumberOfGame() {
+        System.out.println(INPUT_NUMBER_GUIDE_MESSAGE);
         String number = getString();
         Pattern pattern = Pattern.compile(REGEX_VALIDATION);
         Matcher matcher = pattern.matcher(number);
